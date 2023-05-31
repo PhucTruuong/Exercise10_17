e599dcf52e3b30a9042aee6906c353ece6f719ae
PhucTruuong <133474302+PhucTruuong@users.noreply.github.com>
2023-05-31 11:35:17 +0700

    Exercise 10

---
+++ src/components/PlayersPresentation.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+
+export default function PlayersPresentation({players}){
+    console.log(players);
+    const [player, setPlayer] = useState([])
+    return (
+        <div className='container'>
+        {players.map((player)=>(
+            <div className='column' key={player.id}>
+                <div className='card'>
+                    <img alt='' src={player.img}/>
+                    <div className='description'>
+                        <h3>{player.name}</h3>
+                        <p className='title'>{player.club}</p>
+                    </div>
+                    <p><a href='#popup1' id='openPopUp'><button onClick={()=>{setPlayer(player)}}>Detail</button></a></p>
+                    </div>
+                </div>  
+            ))}  
+            <div id='popup1' className='overlay'>
+                <div className='popup'>
+                    <img src={player.img} alt=''/>
+                    <h2>{player.name}</h2>
+                    <a className='close' href="#a">&times;</a>
+                    <div className='content'>
+                        {player.info}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
