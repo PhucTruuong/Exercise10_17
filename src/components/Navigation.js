2d21e91862219e23613d26705fe76e17286bedc1
PhucTruuong <133474302+PhucTruuong@users.noreply.github.com>
2023-06-16 17:06:36 +0700

    React Router

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,6 @@ export default function Navigation(){
         <div>
             <nav>
                 <ul className="menu">
-                
                     <li><a className='active' href='#home'>Home</a></li>
                     <li><a href='#news'>News</a></li>
                     <li><a href='#about'>About</a></li>
--- src/components/Players.js
+++ src/components/Players.js
@@ -17,8 +17,7 @@ function Players() {
             <p className="btn">
               <button onClick={() => {setPlayer(player);}}>
                 <Link to={`detail/${player.id}`}>
-                  Detail  
-                  
+                  Detail 
                 </Link>
               </button>
             </p>
